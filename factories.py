49074f16a69808aba0e4ed1c4d915cf629b76dd6
Polly-25 <polinapolina.smirnova@yandex.ru>
2026-05-17 00:11:50 +0300

    Update factories.py

--- factories.py
+++ factories.py
@@ -1,20 +1,27 @@
 import random
+
 import factory.alchemy
 
-from models import db, Client, Parking
+from .models import Client, Parking, db
+
+
 class ClientFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = Client
         sqlalchemy_session = db.session
-    name = factory.Faker('first_name')
-    surname = factory.Faker('last_name')
-    credit_card = factory.Faker('credit_card_number')
-    car_number = factory.Faker('license_plate')
+
+    name = factory.Faker("first_name")
+    surname = factory.Faker("last_name")
+    credit_card = factory.Faker("credit_card_number")
+    car_number = factory.Faker("license_plate")
+
 
 class ParkingFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = Parking
         sqlalchemy_session = db.session
-    address = factory.Faker('address')
-    opened = factory.Faker('boolean')
+
+    address = factory.Faker("address")
+    opened = factory.Faker("boolean")
     count_places = factory.LazyAttribute(lambda x: random.randrange(50, 200))
+
