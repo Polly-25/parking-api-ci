c14d08e47b3be2c0d967f3f936aac09fed14417a
Polly-25 <polinapolina.smirnova@yandex.ru>
2026-05-17 00:10:39 +0300

    Update test_parking_exit.py

--- tests/test_parking_exit.py
+++ tests/test_parking_exit.py
@@ -1,4 +1,6 @@
 import pytest
+
+
 @pytest.mark.parking
 def test_parking_exit(client):
 
@@ -6,7 +8,7 @@ def test_parking_exit(client):
         "name": "Выездов",
         "surname": "Тестовый",
         "credit_card": "9999888877776666",
-        "car_number": "B321CD"
+        "car_number": "B321CD",
     }
     client_resp = client.post("/clients", json=client_data)
     assert client_resp.status_code == 201
@@ -15,23 +17,17 @@ def test_parking_exit(client):
         "address": "Улица Парковая, 2",
         "opened": True,
         "count_places": 15,
-        "count_available_places": 15
+        "count_available_places": 15,
     }
     parking_resp = client.post("/parkings", json=parking_data)
     assert parking_resp.status_code == 201
     parking_id = parking_resp.get_json()["id"]
 
-    enter_data = {
-        "client_id": client_id,
-        "parking_id": parking_id
-    }
+    enter_data = {"client_id": client_id, "parking_id": parking_id}
     enter_resp = client.post("/clients_parkings", json=enter_data)
     assert enter_resp.status_code == 201
 
-    exit_data = {
-        "client_id": client_id,
-        "parking_id": parking_id
-    }
+    exit_data = {"client_id": client_id, "parking_id": parking_id}
     exit_resp = client.delete("/clients_parkings", json=exit_data)
     assert exit_resp.status_code == 200
 
