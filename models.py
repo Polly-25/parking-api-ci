3ede2c90d1697aeac9d899a624551b2f340caf3f
Polly-25 <polinapolina.smirnova@yandex.ru>
2026-05-17 01:17:53 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,13 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import (
-    Boolean,
-    Column,
-    DateTime,
-    ForeignKey,
-    Integer,
-    String,
-    UniqueConstraint,
-)
+from sqlalchemy import (Boolean, Column, DateTime, ForeignKey, Integer, String,
+                        UniqueConstraint)
 
 db = SQLAlchemy()
 
