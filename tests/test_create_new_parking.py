31d8d0b38a88da9721a4a300a5c5a77787a5d35d
Polly-25 <polinapolina.smirnova@yandex.ru>
2026-05-17 00:07:28 +0300

    Update test_create_new_parking.py

--- tests/test_create_new_parking.py
+++ tests/test_create_new_parking.py
@@ -3,14 +3,17 @@ def test_create_new_parking(client):
         "address": "Примерная улица, 10",
         "opened": True,
         "count_places": 20,
-        "count_available_places": 20
+        "count_available_places": 20,
     }
-    response = client.post('/parkings', json=new_parking_data)
+    response = client.post("/parkings", json=new_parking_data)
     assert response.status_code == 201
 
     data = response.get_json()
-    assert data['address'] == new_parking_data['address']
-    assert data['opened'] == new_parking_data['opened']
-    assert data['count_places'] == new_parking_data['count_places']
-    assert data['count_available_places'] == new_parking_data['count_available_places']
-    assert 'id' in data
+    assert data["address"] == new_parking_data["address"]
+    assert data["opened"] == new_parking_data["opened"]
+    assert data["count_places"] == new_parking_data["count_places"]
+    expected_count = new_parking_data["count_available_places"]
+    actual_count = data["count_available_places"]
+
+    assert actual_count == expected_count
+    assert "id" in data
